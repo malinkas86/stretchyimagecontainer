b929a65215220e3bd8eac9f422c2a7083769e33b
Malinka S <malinka@prolificinteractive.com>
2018-12-11 20:10:08 -0800

    Initial Commit

---
+++ StretchyImageContainer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StretchyImageContainer
+//
+//  Created by Malinka S on 12/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
