4ca4c812940736b00a6beac19f8a9b5b4ff45a6c
Malinka S <>
2018-12-15 16:24:10 -0800

    Stretchy image container example

--- StretchyImageContainer/ViewController.swift
+++ StretchyImageContainer/ViewController.swift
@@ -7,13 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    @IBOutlet private weak var imageContainerScrollView: UIScrollView!
+    @IBOutlet private weak var imageStackView: UIStackView!
+
+    private var imageWidth: CGFloat!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
 
 }
 
